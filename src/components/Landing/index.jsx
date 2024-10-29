5c42fdecd793525da87122da3363cb51cad7f016
Rezvaneh <rezvaneh.333@gmail.com>
2024-10-29 11:05:55 +0330

    fix landing

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -4,33 +4,15 @@ import { Modal, Button, MantineProvider, NavLink } from "@mantine/core";
 import Auth from "../Auth";
 
 const Landing = () => {
-  const [authModal, setAuthModal] = useState(false);
   return (
-    <>
-      <MantineProvider>
-        <Modal
-          title="عنوان"
-          className="dirAuth"
-          opened={authModal}
-          onClose={() => setAuthModal(false)}
-          withCloseButton={true}
-          radius={24}
-        >
-          {/* <div class="w-[356px] h-[156px] mb-[15px] rounded-[50px] border-[1px] border-[#CFD8DC]"> */}
-          {/* {authModal === true && <Auth setAuthModal={setAuthModal} />} */}
-          <Auth />
-
-          {/* </div> */}
-        </Modal>
-        <Button
-          onClick={() => {
-            setAuthModal(true);
-          }}
-        >
-          ورود
-        </Button>
-      </MantineProvider>
-    </>
+    <div>
+    <HeroSection />
+    <ServiceHolder/>
+    <CourseHolder/>
+    <CourseCategory/>
+    <TeacherHolder/>
+    <NewsHolder/>
+  </div>
   );
 };
 
